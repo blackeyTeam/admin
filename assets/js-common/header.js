ea844d0f8981c3ec7a46f9e8db65e60061fc0ae0
befairytale@163.com <befairytale@163.com>
2018-04-08 21:34:17 +0800

    commit

--- assets/js-common/header.js
+++ assets/js-common/header.js
@@ -19,32 +19,32 @@ var header_l = '<div class="sidebar-collapse">'+
                             'PL'+
                         '</div>'+
                     '</li>'+
-                    '<li>'+
-                        '<a><i class="fa fa-cogs"></i> <span class="nav-label">系统设置</span> <span class="fa arrow"></span></a>'+
-                        '<ul class="nav nav-second-level collapse">'+
-                            '<li class="system-admin"><a href="../system/admin.html">管理员管理</a></li>'+
-                            '<li class="system-role"><a href="../system/role.html">成员管理</a></li>'+
-                            '<li class="system-area"><a href="../system/area.html">管理区域</a></li>'+
-                        '</ul>'+
-                    '</li>'+
+                    // '<li>'+
+                    //     '<a><i class="fa fa-cogs"></i> <span class="nav-label">系统设置</span> <span class="fa arrow"></span></a>'+
+                    //     '<ul class="nav nav-second-level collapse">'+
+                    //         '<li class="system-admin"><a href="../system/admin.html">管理员管理</a></li>'+
+                    //         '<li class="system-role"><a href="../system/role.html">成员管理</a></li>'+
+                    //         '<li class="system-area"><a href="../system/area.html">管理区域</a></li>'+
+                    //     '</ul>'+
+                    // '</li>'+
                     '<li>'+
                         '<a><i class="fa fa-users"></i> <span class="nav-label">商户管理</span> <span class="fa arrow"></span></a>'+
                         '<ul class="nav nav-second-level collapse">'+
                             '<li class="vendor-list"><a href="../vendor/vendor.html">商户列表</a></li>'+
-                            '<li class="vendor-auth"><a href="../vendor/vendor-auth.html">商户审核</a></li>'+
-                            '<li class="vendor-type"><a href="../vendor/vendor-type.html">商户类型</a></li>'+
+                            // '<li class="vendor-auth"><a href="../vendor/vendor-auth.html">商户审核</a></li>'+
+                            // '<li class="vendor-type"><a href="../vendor/vendor-type.html">商户类型</a></li>'+
                         '</ul>'+
                     '</li>'+
-                    '<li>'+
-                        '<a><i class="fa fa-cubes"></i> <span class="nav-label">商品管理</span> <span class="fa arrow"></span></a>'+
-                        '<ul class="nav nav-second-level collapse">'+
-                            '<li class="goods-classify"><a href="../goods/goods-classify.html">商品分类</a></li>'+
-                            '<li class="goods-brand"><a href="../goods/goods-brand.html">品牌管理</a></li>'+
-                            '<li class="goods-param"><a href="../goods/goods-param.html">参数管理</a></li>'+
-                            '<li class="goods-info"><a href="../goods/goods-info.html">商品管理</a></li>'+
-                            '<li class="goods-price"><a href="../goods/goods-price.html">商品定价</a></li>'+
-                        '</ul>'+
-                    '</li>'+
+                    // '<li>'+
+                    //     '<a><i class="fa fa-cubes"></i> <span class="nav-label">商品管理</span> <span class="fa arrow"></span></a>'+
+                    //     '<ul class="nav nav-second-level collapse">'+
+                    //         '<li class="goods-classify"><a href="../goods/goods-classify.html">商品分类</a></li>'+
+                    //         '<li class="goods-brand"><a href="../goods/goods-brand.html">品牌管理</a></li>'+
+                    //         '<li class="goods-param"><a href="../goods/goods-param.html">参数管理</a></li>'+
+                    //         '<li class="goods-info"><a href="../goods/goods-info.html">商品管理</a></li>'+
+                    //         '<li class="goods-price"><a href="../goods/goods-price.html">商品定价</a></li>'+
+                    //     '</ul>'+
+                    // '</li>'+
                     '<li>'+
                         '<a><i class="fa fa-home"></i> <span class="nav-label">店铺管理</span> <span class="fa arrow"></span></a>'+
                         '<ul class="nav nav-second-level collapse">'+
@@ -52,35 +52,35 @@ var header_l = '<div class="sidebar-collapse">'+
                             '<li class="shops-list"><a href="../shops/shops-list.html">店铺列表</a></li>'+
                         '</ul>'+
                     '</li>'+
-                    '<li>'+
-                        '<a><i class="fa fa-thumbs-up"></i> <span class="nav-label">推荐管理</span> <span class="fa arrow"></span></a>'+
-                        '<ul class="nav nav-second-level collapse">'+
-                            '<li class="recommend-position"><a href="../recommend/recommend-position.html">推荐位置</a></li>'+
-                            '<li class="recommend-list"><a href="../recommend/advertisement.html">推荐列表</a></li>'+
-                        '</ul>'+
-                    '</li>'+
+                    // '<li>'+
+                    //     '<a><i class="fa fa-thumbs-up"></i> <span class="nav-label">推荐管理</span> <span class="fa arrow"></span></a>'+
+                    //     '<ul class="nav nav-second-level collapse">'+
+                    //         '<li class="recommend-position"><a href="../recommend/recommend-position.html">推荐位置</a></li>'+
+                    //         '<li class="recommend-list"><a href="../recommend/advertisement.html">推荐列表</a></li>'+
+                    //     '</ul>'+
+                    // '</li>'+
                     '<li>'+
                         '<a><i class="fa fa-copy"></i> <span class="nav-label">订单管理</span> <span class="fa arrow"></span></a>'+
                         '<ul class="nav nav-second-level collapse">'+
                             '<li class="order-list"><a href="../order/order-list.html">订单列表</a></li>'+
                         '</ul>'+
                     '</li>'+
-                    '<li>'+
-                        '<a><i class="fa fa-bus"></i> <span class="nav-label">物流管理</span> <span class="fa arrow"></span></a>'+
-                        '<ul class="nav nav-second-level collapse">'+
-                            '<li class="logistics-servicer"><a href="../logistics/servicer.html">物流商</a></li>'+
-                            '<li class="logistics-tmpl"><a href="../logistics/tmpl.html">运费模板</a></li>'+
-                            '<li class="logistics-address"><a href="../logistics/address.html">地址库</a></li>'+
-                        '</ul>'+
-                    '</li>'+
-                    '<li>'+
-                        '<a><i class="fa fa-shield"></i> <span class="nav-label">库存管理</span> <span class="fa arrow"></span></a>'+
-                        '<ul class="nav nav-second-level collapse">'+
-                            '<li class="stock-storage"><a href="../stock/storage.html">仓库管理</a></li>'+
-                            '<li class="stock-stock"><a href="../stock/stock.html">库存管理</a></li>'+
-                            '<li class="stock-Sync"><a href="../stock/Sync.html">同步</a></li>'+
-                        '</ul>'+
-                    '</li>'+
+                    // '<li>'+
+                    //     '<a><i class="fa fa-bus"></i> <span class="nav-label">物流管理</span> <span class="fa arrow"></span></a>'+
+                    //     '<ul class="nav nav-second-level collapse">'+
+                    //         '<li class="logistics-servicer"><a href="../logistics/servicer.html">物流商</a></li>'+
+                    //         '<li class="logistics-tmpl"><a href="../logistics/tmpl.html">运费模板</a></li>'+
+                    //         '<li class="logistics-address"><a href="../logistics/address.html">地址库</a></li>'+
+                    //     '</ul>'+
+                    // '</li>'+
+                    // '<li>'+
+                    //     '<a><i class="fa fa-shield"></i> <span class="nav-label">库存管理</span> <span class="fa arrow"></span></a>'+
+                    //     '<ul class="nav nav-second-level collapse">'+
+                    //         '<li class="stock-storage"><a href="../stock/storage.html">仓库管理</a></li>'+
+                    //         '<li class="stock-stock"><a href="../stock/stock.html">库存管理</a></li>'+
+                    //         '<li class="stock-Sync"><a href="../stock/Sync.html">同步</a></li>'+
+                    //     '</ul>'+
+                    // '</li>'+
                 '</ul>'+
             '</div>';
 document.getElementById('header-l').innerHTML = header_l;
